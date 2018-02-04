dc6b53b88a87378299c82fb91a375495e4cb6b3f
Wendy <tell.wendy@gmail.com>
2018-02-03 22:29:45 -0800

    Overlay component

--- src/Lesson.js
+++ src/Lesson.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import * as MyScriptJS from 'myscript/src/myscript';
+import * as MyScriptJS from "myscript/src/myscript";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 import data from "./data";
@@ -17,114 +17,135 @@ const CharacterContainer = styled.div`
   align-items: center;
 `;
 
+const Overlay = styled.div`
+  position: absolute;
+  top: 0;
+  border: 1px solid black;
+  z-index: 50;
+  min-width: 50%;
+  min-height: 50%;
+  background-color: #000;
+  border: 1pv solid #efefef;
+  font-size: 2em;
+`;
+
 class Lesson extends Component {
   state = {
-    currentLevel: 0
-  }
+    currentLevel: 0,
+    showOverlay: false
+  };
 
   componentWillMount() {
-    
     const lessonNum = this.props.location.pathname.slice(-1);
     const lessonData = data[lessonNum - 1];
-    console.log(lessonData, "<<<DATA")
+    console.log(lessonData, "<<<DATA");
     this.setState({
       lessonNum,
       lessonData
-    })
-
+    });
   }
 
   componentDidMount() {
+    var editorElement = document.getElementById("editor");
+    const exportedIsAnswer = this.exportedIsAnswer;
 
-    var editorElement = document.getElementById('editor');
-    const exportedIsAnswer = this.exportedIsAnswer;;
-
-    MyScriptJS.register(this.refs.editor, {
-      recognitionParams: {
-        type: 'TEXT',
-        v4: {
-          lang: "zh_HK",
-          text: {
-            guides: {
-              enable: false
+    MyScriptJS.register(
+      this.refs.editor,
+      {
+        recognitionParams: {
+          type: "TEXT",
+          v4: {
+            lang: "zh_HK",
+            text: {
+              guides: {
+                enable: false
+              }
             }
+          },
+          server: {
+            applicationKey: "e4356f44-42bf-4eaf-97c5-504e8d0e722c",
+            hmacKey: "d3c3fb0e-ee79-434b-9611-ac223a23265b"
           }
-        },
-        server: {
-          applicationKey: 'e4356f44-42bf-4eaf-97c5-504e8d0e722c',
-          hmacKey: 'd3c3fb0e-ee79-434b-9611-ac223a23265b'
+        }
+      },
+      undefined,
+      {
+        ".text": {
+          "font-size": 50
         }
       }
-    }, undefined, {
-      '.text': {
-        'font-size': 50
-      }
-    });
+    );
 
-    editorElement.addEventListener('exported', function (evt) {
+    editorElement.addEventListener("exported", function(evt) {
       var exports = evt.detail.exports;
-      console.log("EXPORTS>>>", exports)
-      if (exports && exports['text/plain']) {
+
+      if (exports && exports["text/plain"]) {
         exportedIsAnswer(exports);
-      } else {
-        // resultElement.innerHTML = '';
       }
     });
-
   }
 
   handleClick = event => {
     this.setState(state => ({
-      currentLevel: state.currentLevel++
-    }))
-  }
+      currentLevel: ++state.currentLevel,
+      showOverlay: false
+    }));
+  };
 
   exportedIsAnswer = exports => {
-    console.log("IN exportedIsAnswer")
-    const chars = exports['text/plain'];
-    const { lessonData } = this.state;
-    if (lessonData.chi === chars) {
-      console.log("MATCH!!")
+    const chars = exports["text/plain"];
+    const { lessonData, lessonNum, currentLevel } = this.state;
+
+    if (lessonData.levels[currentLevel].chi === chars) {
+      console.log("MATCH!!");
+      this.setState({ showOverlay: true });
     }
-  }
+  };
 
   render() {
-
     const editorStyle = {
-      'minWidth': '500px',
-      'minHeight': '400px',
-      'border': '1px solid #000',
-      'maxWidth': '100%'
+      minWidth: "500px",
+      minHeight: "400px",
+      border: "1px solid #000",
+      maxWidth: "100%"
     };
 
-    const {lessonNum, lessonData, currentLevel} = this.state;
+    const { lessonNum, lessonData, currentLevel, showOverlay } = this.state;
 
     return (
-    <div>
-    <Link to="/lessons" style={{float: "left"}}>Back</Link>
-    <h2>Lesson {lessonNum} - {lessonData.title}</h2>
-    <CharacterContainer>
-      <div style={{padding: "0 5em"}}>
-        <h1>{lessonData.levels[currentLevel].chi}</h1>
-        <h1>{lessonData.levels[currentLevel].eng}</h1>
+      <div style={{position: "relative"}}>
+        <Link to="/lessons" style={{ float: "left" }}>
+          Back
+        </Link>
+        <h2>
+          Lesson {lessonNum} - {lessonData.title}
+        </h2>
+        <CharacterContainer>
+          <div style={{ padding: "0 5em" }}>
+            <h1>{lessonData.levels[currentLevel].chi}</h1>
+            <h1>{lessonData.levels[currentLevel].eng}</h1>
+          </div>
+          <div>
+            {lessonData.levels[currentLevel].gif.map(char => {
+              return (
+                <img src={char} style={{ width: "50px", height: "50px" }} />
+              );
+            })}
+          </div>
+        </CharacterContainer>
+        <WritingPad>
+        <i className="fas fa-undo"></i>
+          <div id="editor" style={editorStyle} ref="editor" />
+        </WritingPad>
+        {showOverlay && (
+          <Overlay>
+            <div>check mark here</div>
+              <div onClick={this.handleClick}>Next</div>
+          </Overlay>
+        )}
       </div>
-      <div>
-        {lessonData.levels[currentLevel].gif.map(char => {
-          return <img src={char} style={{width: "50px", height: "50px"}}/>
-        })}
-      </div>
-    </CharacterContainer>
-    <WritingPad>
-      <div id="editor" style={editorStyle} ref="editor"></div>
-    </WritingPad>
-    <div>
-      this is overlay
-      <div onClick={this.handleClick}>Next</div>
-    </div>
-    </div>
-      );
+    );
   }
-} 
- 
+}
 
 export default Lesson;
