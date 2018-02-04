6be25db64770e9ddf8a8dcf6c1c29b87455cdd90
ClarabelleCheng-Yue <cachengyue@gmail.com>
2018-02-04 01:59:43 -0800

    Add schema

--- server/app.js
+++ server/app.js
@@ -15,30 +15,37 @@ mongoose.connect('mongodb://localhost/test');
 
 var db = mongoose.connection;
 
-db.on( 'error', console.error.bind( console, 'connection error:' ) );
+db.on('error', console.error.bind(console, 'connection error:'));
 
 var Schema = mongoose.Schema;
 
 var lessonSchema = new Schema({
-    // need to build schema - waiting on Wendy
-  name: String,
+  // need to build schema - waiting on Wendy
+  lesson: Number,
+  title: String,
+  levels: [{
+    eng: String,
+    chi: String,
+    gif: String[],
+    audio: String[]
+  }]
 });
 
 var Lesson = mongoose.model('Lesson', lessonSchema)
 
-Lesson.collection.insertMany(data, function(err,r) {
-    if (error) {
-        console.error(error);
-    } else {
-        console.log(r.insertedCount + " has been saved!");
-    }
+Lesson.collection.insertMany(data, function (err, r) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(r.insertedCount + " has been saved!");
+  }
 });
 
 // GET INFO
-app.get('/lessons', function(req, res) {
-    Lesson.find({}, function(err, lesson) {
-        res.send(lesson);  
-    });
+app.get('/lessons', function (req, res) {
+  Lesson.find({}, function (err, lesson) {
+    res.send(lesson);
+  });
 });
 
 app.listen(8000);
