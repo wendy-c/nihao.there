1b742088ab2904947c783ab83a5667d109e04153
ClarabelleCheng-Yue <cachengyue@gmail.com>
2018-02-03 20:29:49 -0800

    Add box shadow

--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,21 @@
 import React, { Component } from "react";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
+import { ImpactfulHeader, Background } from './styles';
 
 export default class Home extends Component {
   render() {
-    return(
-        <div className="home-container">
-          <div className="welcome">Welcome!</div>
-          <Link to="/lessons">Click here to begin lessons</Link>
-        </div>
-      );
+    return (
+      <div className="home-container">
+        <Background color='red'>
+          <ImpactfulHeader>
+            <h1>Ni hao there! </h1>
+            <div subheader><h2>你 好！</h2></div>
+          </ImpactfulHeader>
+        </Background>
+
+        <div className="welcome">Welcome!</div>
+        <Link to="/lessons">Click here to begin lessons</Link>
+      </div>
+    );
   }
 }
